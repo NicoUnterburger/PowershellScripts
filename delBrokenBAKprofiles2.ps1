d85df155104016d3487f2dec428d7660c2f2ef6e
Nico Unterburger <nico-unterburger@live.de>
2024-06-25 08:40:55 +0200

    Add files via upload

---
+++ delBrokenBAKprofiles2.ps1
@@ -0,0 +1,8 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+gci 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' | %{
+    $hklmpath = $_.name -replace "HKEY_LOCAL_MACHINE","hklm:"
+    $path =  (Get-ItemProperty -LiteralPath $hklmpath -Name ProfileImagePath).ProfileImagePath
+    if(-not(test-path $path)) {
+            Remove-Item -Path $hklmpath
+    }
+}
